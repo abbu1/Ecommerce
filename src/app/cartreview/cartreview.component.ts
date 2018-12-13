bd065679cdf5eb896a26b671137c5655db744848
abbu1 <30627182+abbu1@users.noreply.github.com>
2018-12-13 21:20:16 +0530

    Add files via upload

--- src/app/cartreview/cartreview.component.ts
+++ src/app/cartreview/cartreview.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {DataserviceService} from '../dataservice.service';
 
 @Component({
   selector: 'app-cartreview',
@@ -7,16 +8,19 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 })
 export class CartreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataservice : DataserviceService) {}
 //@Input().reviews:number;
 //prod:number=300;
-mySubjectVal: string;
-  @Input('myInputVal') myInputVal: string;
-  @Output('myOutputVal') myOutputVal = new EventEmitter();
+//mySubjectVal: string;
+  //@Input('myInputVal') myInputVal: string;
+  //@Output('myOutputVal') myOutputVal = new EventEmitter();
 //addNewProduct = function(product){
   
 //}
   ngOnInit() {
+    //shashwati madam code
+  this.dataservice.setLoaderStatus(true);
+  //shashwati madam code ends here
   }
-
+  
 }
